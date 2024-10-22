c7eb22a3a896ac3a68e22841868c6490bcedd6bc
asma <Asma.mo@outlook.com>
2024-10-22 14:50:27 +0300

    Initial Commit

---
+++ learningJourneyApp/learningJourneyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningJourneyAppApp.swift
+//  learningJourneyApp
+//
+//  Created by Asma on 22/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct learningJourneyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ learningJourneyAppTests/learningJourneyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  learningJourneyAppTests.swift
+//  learningJourneyAppTests
+//
+//  Created by Asma on 22/10/2024.
+//
+
+import Testing
+@testable import learningJourneyApp
+
+struct learningJourneyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
